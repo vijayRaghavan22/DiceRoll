df98f5800520c43e6d3451fdb267eeaa451b1525
vijayraghavan <m.vijayraghavan22@gmail.com>
2022-08-31 20:48:05 -0400

    Added conditional statement to declare interactive messages

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -10,11 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var myFIrstDice : Dice = Dice(4,"MyFIrstDice");
+        val luckyNumber=4;
 
         val rollButton : Button = findViewById(R.id.button);
         rollButton.setOnClickListener {
             val myViewIs : TextView = findViewById(R.id.textView);
-            myViewIs.text= myFIrstDice.diceRoll().toString();
+            val rollResult=myFIrstDice.diceRoll();
+            when(rollResult){
+                luckyNumber -> {
+                    myViewIs.text="You WIN its a ${rollResult.toString()}";
+                    println("Winner found");
+                };
+                1 -> myViewIs.text= "So sorry! You rolled a 1. Try again!";
+                2 -> myViewIs.text= "Sadly, you rolled a 2. Try again!"
+                3 -> myViewIs.text= "Unfortunately, you rolled a 3. Try again!"
+                5 -> myViewIs.text= "Don't cry! You rolled a 5. Try again!"
+                6 -> myViewIs.text= "Apologies! You rolled a 6. Try again!"
+            }
         }
     }
 }
